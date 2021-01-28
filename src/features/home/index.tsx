30a4516f61e023f54823bec79ad0d8e89734d17e
Yen Pou Seow <alexis11894@hotmail.com>
2021-01-29 00:00:11 +1100

    Added Homepage (#14)

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -3,6 +3,12 @@ import {
   AuthenticationRepositoryContext,
   AuthenticationRepositoryContextInterface,
 } from '../../context/authentication';
+import { Space, Typography, Image, Button } from 'antd';
+import { HomeWrapper } from './styled';
+import HomeHowto from './home-howto';
+import HomeAboutUs from './home-about';
+import HomeContactUs from './home-contactus';
+
 
 const Home = () => {
   const authRepo = useContext<AuthenticationRepositoryContextInterface>(
@@ -13,7 +19,33 @@ const Home = () => {
     .get()
     .okOrDefault();
 
-  return <p>hello {loggedInUserId}</p>;
+  return (
+    <HomeWrapper>
+      <Space className='whitebg' direction='vertical'>
+        <Typography.Title>Coming Soon...</Typography.Title>
+        <Image 
+          src='https://i.imgur.com/vgn9VFo.png' 
+          alt='Summer BBQ Logo' 
+          className='imagewidth'
+        />
+        <Typography.Title level={4}>
+          Show off your moves and join in on the hottest DDR seasonal event starting February 2021. Pre-register your account today!
+        </Typography.Title>
+        <Button type="primary">Sign Up</Button>
+      </Space>
+
+      <Space className='darkbg' direction='vertical'>
+        <HomeAboutUs />
+      </Space>
+      <Space className='whitebg' direction='vertical'>
+        <HomeHowto />
+      </Space>
+      <Space className='darkbg' direction='vertical'> 
+        <HomeContactUs />
+      </Space>
+    </HomeWrapper>
+       
+        )
 };
 
 export default Home;
