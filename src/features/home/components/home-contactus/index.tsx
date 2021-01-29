b05847c513559e257785118c6b991950423642a5
Yen Pou Seow <alexis11894@hotmail.com>
2021-01-29 11:47:29 +1100

    Add links to social media icons [#35]

--- src/features/home/components/home-contactus/index.tsx
+++ src/features/home/components/home-contactus/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Space, Typography, Image } from 'antd';
 
 const HomeContactUs = () => {
+    const { Link } = Typography;
     return (
         <>
             <Typography.Title style={{color: '#f2f2f2'}}>Want more information?</Typography.Title>
@@ -9,9 +10,27 @@ const HomeContactUs = () => {
                 If you have any inquiries or are interested in getting involved with the AUSDDREvents team, feel free to get in touch with us via the following services:
             </Typography.Paragraph>
             <Space direction='horizontal'>
-                <Image src="https://i.imgur.com/DG4HgJn.png" className='smallimagewidth'/>
-                <Image src="https://i.imgur.com/zGzeQ7G.png" className='smallimagewidth'/>
-                <Image src="https://i.imgur.com/j1aQgYK.png" className='smallimagewidth'/>
+            <Link href='https://www.facebook.com/groups/2053507828081261'>
+                        <Image 
+                            src="https://i.imgur.com/DG4HgJn.png" 
+                            className='smallimagewidth'
+                            preview={false}
+                        />
+                    </Link>
+                    <Link href='https://discord.gg/DsKWPxY4V7' title='discord'>
+                        <Image 
+                            src="https://i.imgur.com/zGzeQ7G.png" 
+                            className='smallimagewidth'
+                            preview={false}
+                        />
+                    </Link>
+                    <Link href='mailto:ausddrevents@gmail.com' title='gmail'>
+                        <Image 
+                            src="https://i.imgur.com/j1aQgYK.png" 
+                            className='smallimagewidth'
+                            preview={false}
+                        />
+                    </Link>
             </Space>
             <Typography.Paragraph style={{color: '#f2f2f2'}}>    
                 We’re looking forward to seeing your moves on the dance floor!
