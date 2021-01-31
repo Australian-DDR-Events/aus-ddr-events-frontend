734ca32a0b1ba6d60bcdff3a84c37f83d70c6357
darqsss <78127642+darqsss@users.noreply.github.com>
2021-01-31 23:45:56 +1100

    Update index.tsx

--- src/features/how-to/components/step-2/index.tsx
+++ src/features/how-to/components/step-2/index.tsx
@@ -10,7 +10,7 @@ const Step2 = () => {
           <Title level={4}>Step 2: Find ingredients</Title>
           <Typography.Text>
             To start cooking you will need to find ingredients. Submit scores
-            for individual songs via the Score Submission Page to earn
+            for the listed individual songs on the Score Submission Page to earn
             ingredients for cooking. Ingredient quality is based on the EX score
             of your score submission.
           </Typography.Text>
