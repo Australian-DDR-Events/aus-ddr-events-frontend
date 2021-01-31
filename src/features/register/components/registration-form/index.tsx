bfcee7cf8f5ff904d62c6ace9fba4c372ebfccfa
Yen Pou Seow <alexis11894@hotmail.com>
2021-01-31 20:51:46 +1100

    Add minimum password characters  (DDR-55)

--- src/features/register/components/registration-form/index.tsx
+++ src/features/register/components/registration-form/index.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Form, Input, Tooltip, Button } from 'antd';
+import { Form, Input, Tooltip, Button, Typography } from 'antd';
 import { QuestionCircleOutlined } from '@ant-design/icons';
 import { useLocation } from 'wouter';
 import { DefaultUser, UserRepositoryContext } from 'context/user';
@@ -7,16 +7,21 @@ import {
   AuthenticationRepositoryContext,
   AuthenticationRepositoryContextInterface,
 } from 'context/authentication';
+import { StyledForm } from './styled';
 
 const formItemLayout = {
   labelCol: {
-    xs: { span: 24 },
-    sm: { span: 4 },
+    xs: { span: 8 },
+    sm: { span: 8 },
   },
   wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 24 },
+    xs: { span: 16 },
+    sm: { span: 16 },
   },
+};
+
+const buttonLayout = {
+  wrapperCol: { offset: 8, span: 16 },
 };
 
 const RegistrationForm = () => {
@@ -43,96 +48,111 @@ const RegistrationForm = () => {
   };
 
   return (
-    <Form
-      {...formItemLayout}
-      form={form}
-      name="register"
-      onFinish={onFinish}
-      scrollToFirstError
-    >
-      <Form.Item
-        name="email"
-        label="E-mail"
-        rules={[
-          {
-            type: 'email',
-            message: 'The input is not valid E-mail!',
-          },
-          {
-            required: true,
-            message: 'Please input your E-mail!',
-          },
-        ]}
+    <>
+      <StyledForm
+        direction="vertical"
+        {...formItemLayout}
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        scrollToFirstError
       >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
-        name="confirm"
-        label="Confirm Password"
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Please confirm your password!',
-          },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject(
-                new Error('The two passwords that you entered do not match!'),
-              );
+        <Typography.Title>Registration</Typography.Title>
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: 'email',
+              message: 'The input is not valid E-mail!',
             },
-          }),
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+            {
+              required: true,
+              message: 'Please input your E-mail!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        name="displayName"
-        label={
-          <span>
-            Display Name&nbsp;
-            <Tooltip title="What do you want others to call you?">
-              <QuestionCircleOutlined />
-            </Tooltip>
-          </span>
-        }
-        rules={[
-          {
-            required: true,
-            message: 'Please input your display name!',
-            whitespace: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Register
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          name="confirm"
+          label="Confirm Password"
+          dependencies={['password']}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: 'Please confirm your password!',
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error('The two passwords that you entered do not match!'),
+                );
+              },
+            }),
+
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (value.length >= 8 || getFieldValue('password') >= 8) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error('Must have at least 8 characters!'),
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="displayName"
+          label={
+            <span>
+              Display Name&nbsp;
+              <Tooltip title="What do you want others to call you?">
+                <QuestionCircleOutlined />
+              </Tooltip>
+            </span>
+          }
+          rules={[
+            {
+              required: true,
+              message: 'Please input your display name!',
+              whitespace: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item {...buttonLayout}>
+          <Button type="primary" htmlType="submit">
+            Register
+          </Button>
+        </Form.Item>
+      </StyledForm>
+    </>
   );
 };
 
---
+++ src/features/register/components/registration-form/styled.ts
@@ -0,0 +1,8 @@
+import { Form } from 'antd';
+import styled, { defaultSpacing } from 'types/styled-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const StyledForm = styled(Form)`
+  max-width: ${defaultSpacing * 88}px;
+  padding: 16px;
+`;
