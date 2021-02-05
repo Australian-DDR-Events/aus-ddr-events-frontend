3becb4a9d219c18a97ad5ef879b120de123d63d3
Samuel Tumewa <stumewa@yahoo.com>
2021-02-05 16:22:39 +1100

    Removed irrelevant components from DDR-1

--- src/features/submission/components/song-submission/styled.ts
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const SongSubmissionWrapper = styled.div`
-  padding: 8px;
-`;
--- src/features/submission/index.tsx
+++ src/features/submission/index.tsx
@@ -1,58 +1,9 @@
-import { DownOutlined } from "@ant-design/icons";
-import { Dropdown, Menu, Typography } from "antd";
-import React, { useState } from "react";
-import SongSubmission from "./components/song-submission";
-import { SubmissionSelectionWrapper, SubmissionWrapper } from "./styled";
+import { Typography } from "antd";
+import React from "react";
 
 const Submission = () => {
-  const songs = ['BURNIN\' THE FLOOR',
-    'Holic',
-    'Heron',
-    'Eternal Summer',
-    'RIGHT ON TIME',
-    'Pierce The Sky',
-    'nightbird lost wing',
-    'PRANA',
-    'out of focus',
-    'Magnetic',
-    'Cosy Catastrophe'
-  ]
-  const [ dropdownTitle, setDropdownTitle ] = useState('Select a song');
-  const [ uploadUiDisplay, setUploadUiDisplayed] = useState(false);
-
-  const menu = (
-    <Menu>
-      {
-        songs.map((song, index) => {
-          return (
-            <Menu.Item key={index} onClick={() => updateCurrentSong(song)}>
-              {song}
-            </Menu.Item>
-          )
-        })
-      }
-    </Menu>
-  )
-
-  const updateCurrentSong = (song: string) => {
-    setDropdownTitle(song);
-    setUploadUiDisplayed(true);
-  }
-
   return (
-    <SubmissionWrapper>
-      <SubmissionSelectionWrapper>
-        <Typography.Text>Step 1: </Typography.Text>
-        <Dropdown overlay={menu} trigger={['click']}>
-          <a className="ant-dropdown-link">
-            {dropdownTitle} <DownOutlined />
-          </a>
-        </Dropdown>
-      </SubmissionSelectionWrapper>
-      {uploadUiDisplay && (
-        <SongSubmission />
-      )}
-    </SubmissionWrapper>
+    <Typography.Text>To be added in DDR-1...</Typography.Text>
   );
 }
 
