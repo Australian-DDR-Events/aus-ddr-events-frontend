413dd2b41db687779d08a0a062814f715d4196e2
Yen Pou Seow <alexis11894@hotmail.com>
2021-03-03 15:52:00 +1100

    add getAll to dancerDao

--- src/context/dancer/repository.ts
+++ src/context/dancer/repository.ts
@@ -2,12 +2,13 @@ import { Result } from 'types/result';
 import { Dancer, DancersRepository, DancersDao } from './types';
 
 const dancersRepository = (dao: DancersDao): DancersRepository => {
+  const getAll = (): Promise<Result<Error, Array<Dancer>>> => dao.getAll();
   const get = (id: string): Promise<Result<Error, Dancer>> => dao.get(id);
 
   const update = (user: Dancer): Promise<Result<Error, boolean>> =>
     dao.update(user);
 
-  return { get, update };
+  return { get, update, getAll };
 };
 
 export default dancersRepository;
--- src/features/leaderboard/components/tablecontent/index.tsx
+++ src/features/leaderboard/components/tablecontent/index.tsx
@@ -1,40 +1,31 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { ScoresRepositoryContext } from 'context/scores';
+import { DancersRepositoryContext, DefaultDancer } from 'context/dancer';
+import { DefaultSummer2021Score } from '~/context/scores/constants';
 import { Table } from 'antd';
 
-const TableContent = () => {
-  const dataSource = [
-    {
-      key: '1',
-      name: 'Mike',
-      age: 32,
-      address: '10 Downing Street',
-    },
-    {
-      key: '2',
-      name: 'John',
-      age: 42,
-      address: '10 Downing Street',
-    },
-  ];
+const { Column } = Table;
 
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-  ];
-  return <Table dataSource={dataSource} columns={columns} />;
+const TableContent = () => {
+  const scoresRepo = useContext(ScoresRepositoryContext);
+  const dancersRepo = useContext(DancersRepositoryContext);
+
+  const [scores, setScores] = useState(DefaultSummer2021Score);
+  const [dancers, setDancers] = useState(Array(DefaultDancer));
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (loading) {
+      dancersRepo.dancersRepositoryInstance.getAll().then((dancersRes) => {
+        setDancers(dancersRes.okOrDefault());
+        setLoading(false);
+      });
+    }
+  });
+
+  console.log(dancers);
+
+  return <div>asd</div>;
 };
 
 export default TableContent;
