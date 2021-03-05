204597ca37a2cbb2386953d4e2e2a38835ed992b
Christopher Shand-Gost <christopher.shandgost@myob.com>
2021-03-05 11:19:59 +1100

    Use just firebase app

--- src/context/authentication/firebase-dao.ts
+++ src/context/authentication/firebase-dao.ts
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { Result, Ok, Err, ok, err } from 'types/result';
 import {
   AuthenticationDao,
