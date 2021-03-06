45d53fce00ea754442bb16a5c9d0e88058c5042d
Nhi <nhivn.dev@gmail.com>
2021-03-06 20:27:42 +1100

    Remove submission

--- src/features/submission/types.ts
+++
@@ -1,8 +0,0 @@
-import { Song } from 'context/songs/types';
-import { Ingredient } from 'context/ingredients/types';
-
-export type SongIngredient = {
-  song: Song;
-  ingredient: Ingredient;
-  submitted: boolean;
-};
