a4a481a60ea091e860026f2399a7f106816969d4
Nhi <nhivn.dev@gmail.com>
2021-03-06 20:24:38 +1100

    Fix broken function

--- src/utils/assets/index.ts
+++ src/utils/assets/index.ts
@@ -1,4 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export const getAssetUrl = (imageUrl: string) =>
-`${process.env.ASSETS_URL}${imageUrl}`; = (imageUrl: string) =>
   `${process.env.ASSETS_URL}${imageUrl}`;
