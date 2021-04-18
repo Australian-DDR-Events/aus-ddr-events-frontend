04f67a607e33115a6afc7e6912682a6ec85bd480
Christopher Shand-Gost <christopher.shandgost@myob.com>
2021-04-19 00:44:07 +1000

    Load badges

--- src/features/profile/profile-read-view/profile-tabs/index.tsx
+++ src/features/profile/profile-read-view/profile-tabs/index.tsx
@@ -7,6 +7,7 @@ import {
   useMediaQuery,
 } from '@chakra-ui/react';
 import { BadgesRepositoryContext } from 'context/badges';
+import { DefaultBadge } from 'context/badges/constants';
 import { Badge } from 'context/badges/types';
 import { Dancer } from 'context/dancer';
 import { DishesRepositoryContext } from 'context/dishes';
@@ -15,33 +16,65 @@ import { SongsRepositoryContext } from 'context/songs';
 import React, { useContext, useState } from 'react';
 import { Song } from 'types/core';
 import { DancerGradedDish, DancerGradedIngredient } from 'types/summer2021';
+import { Context as urqlContext, OperationResult } from 'urql';
 
 import BadgesTab from './badges-tab';
 import DishesTab from './dishes-tab';
 import IngredientsTab from './ingredients-tab';
 
+const BADGES_QUERY = `
+query ( $dancerId: ID!) {
+  dancerById (id: $dancerId) {
+    badges {
+      id
+      name
+      description
+      eventId
+      image128
+    }
+  }
+}`;
+
+type BadgesQueryType = {
+  dancerById: {
+    badges: [
+      {
+        id: string;
+        name: string;
+        description: string;
+        eventId: string;
+        image128: string;
+      },
+    ];
+  };
+};
+
 const ProfileTabs = ({ dancer }: { dancer: Dancer }) => {
   // Set up badges tab
-  const badgesRepo = useContext(BadgesRepositoryContext);
   const [dancerBadges, setDancerBadges] = useState(new Array<Badge>());
   const [isBadgesTabLoaded, setIsBadgesTabLoaded] = useState(false);
   const [isBadgesTabLoading, setIsBadgesTabLoading] = useState(true);
-  /**
-   * Load the badges for the user of the profile being viewed
-   * if the badges have not already been loaded
-   * @returns the profile user's badges
-   */
-  const loadBadges = () => {
-    if (isBadgesTabLoaded) return;
 
-    badgesRepo.badgesRepositoryInstance
-      .getForDancerId(dancer.id)
-      .then((badgeResult) => {
-        if (badgeResult.isOk()) {
-          setDancerBadges(badgeResult.value);
-          setIsBadgesTabLoaded(true);
-          setIsBadgesTabLoading(false);
-        }
+  const client = useContext(urqlContext);
+
+  const loadBadges = () => {
+    client
+      .query(BADGES_QUERY, { dancerId: dancer.id })
+      .toPromise()
+      .then((result: OperationResult<BadgesQueryType>) => {
+        if (!result || !result.data) return;
+        setDancerBadges(
+          result.data.dancerById.badges.map((badge) => ({
+            ...DefaultBadge,
+            id: badge.id,
+            name: badge.name,
+            description: badge.description,
+            eventId: badge.eventId,
+            image128: badge.image128,
+          })),
+        );
+        setIsBadgesTabLoaded(true);
+        setIsBadgesTabLoading(false);
       });
   };
 
